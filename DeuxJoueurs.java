54f8171d823b5bbdbeaee20c6d5b6da3e3e5c22a
Julien NOEL <julien.noel5@etu.univ-lorraine.fr>
2019-04-02 13:42:17 +0200

    Simplification de la classe DeeuxJoueurs

--- DeuxJoueurs.java
+++ DeuxJoueurs.java
@@ -23,7 +23,6 @@ public class DeuxJoueurs extends Partie implements Serializable {
         Scanner sc = new Scanner(System.in);
         Scanner sc2 = new Scanner(System.in);
 
-
         System.out.print("Voulez vous chargez la derniere partie en cours ? (Oui(o) / Non(n) : ");
         boolean charger=false;
         if (sc.nextLine().toUpperCase().compareTo("OUI")==0){
@@ -46,8 +45,6 @@ public class DeuxJoueurs extends Partie implements Serializable {
                         j1.retirer(j2,sc);
                     }
                     System.out.println(j2.getGrille().toString());
-
-
                     System.out.print(j2.getNomJoueur().toUpperCase()+" : Entrez le x pour tirer : ");
                     int x2 = sc.nextInt();
                     System.out.print(j2.getNomJoueur().toUpperCase()+" : Entrez le y pour tirer : ");
@@ -58,7 +55,6 @@ public class DeuxJoueurs extends Partie implements Serializable {
                         j2.retirer(j1,sc);
                     }
                     System.out.println(j1.getGrille().toString());
-
                     if (j1.perdu()){
                         break;
                     }
@@ -85,153 +81,93 @@ public class DeuxJoueurs extends Partie implements Serializable {
                 sc2.close();
             }catch (IOException | ClassNotFoundException ioe){
                 System.out.println("Aucun fichier de sauvegarde existant");
-                System.out.print("Joueur 1, entrez votre nom : ");
-                String nomJ1  = sc.nextLine();
-
-                System.out.println(nomJ1.toUpperCase()+ " saisissez les informations pour votre grille : ");
-                Grille g1 = creerGrille(sc);
-                j1 = new Joueur(g1);
-                j1.setNomJoueur(nomJ1);
-                System.out.println(g1.toString());
-
-                System.out.print("Joueur 2, entrez votre nom : ");
-                String nomJ2  = sc2.nextLine();
-                System.out.println(nomJ2.toUpperCase()+ " saisissez les informations pour votre grille : ");
-                Grille g2 = creerGrille(sc);
-                System.out.println(g2.toString());
-                j2 = new Joueur(g2);
-                j2.setNomJoueur(nomJ2);
-
-                System.out.println(nomJ1.toUpperCase()+" placez vos batteaux : ");
-                saisieBateau(sc,j1);
-
-                System.out.println(nomJ2.toUpperCase()+" placez vos batteaux : ");
-                saisieBateau(sc,j2);
-
-                while(!j1.perdu() && !j2.perdu()){
-                    System.out.print(nomJ1.toUpperCase()+" : Entrez le x pour tirer : ");
-                    int x1 = sc.nextInt();
-                    System.out.print(nomJ1.toUpperCase()+" : Entrez le y pour tirer : ");
-                    int y1 = sc.nextInt();
-                    if (x1>=0 && x1<j2.getGrille().getLargeur() && y1>=0 && y1<j2.getGrille().getHauteur()){
-                        j1.tirer(j2, j2.getGrille().gettCases()[x1][y1]);
-                    }else {
-                        j1.retirer(j2,sc);
-                    }
-                    System.out.println(g2.toString());
-
-
-                    System.out.print(nomJ2.toUpperCase()+" : Entrez le x pour tirer : ");
-                    int x2 = sc.nextInt();
-                    System.out.print(nomJ2.toUpperCase()+" : Entrez le y pour tirer : ");
-                    int y2 = sc.nextInt();
-                    if (x2>=0 && x2<j1.getGrille().getLargeur() && y2>=0 && y2<j1.getGrille().getHauteur()){
-                        j2.tirer(j1, j1.getGrille().gettCases()[x2][y2]);
-                    }else {
-                        j2.retirer(j1,sc);
-                    }
-                    System.out.println(g1.toString());
-
-
-                    if (j1.perdu()){
-                        break;
-                    }
-                    if (j2.perdu()){
-                        break;
-                    }
-                    System.out.println("Voulez-vous sauvegarder et quitter votre partie ? (Oui(o) / Non(n) : ");
-                    boolean sauvegarder=false;
-                    if (sc2.nextLine().toUpperCase().compareTo("OUI")==0){
-                        sauvegarder=true;
-                    }
-                    if (sauvegarder){
-                        sauvegarderDeuxJoueurs("saveDeuxJoueurs",this);
-                        break;
-                    }
-                }
-                if (j1.perdu()){
-                    System.out.println(nomJ2.toUpperCase()+" a gagne !");
-                }
-                if (j2.perdu()){
-                    System.out.println(nomJ1.toUpperCase()+" a gagne !");
-                }
-                sc.close();
-                sc2.close();
+                partieDeuxJoueurs(sc, sc2);
             }
         }else{
-            System.out.print("Joueur 1, entrez votre nom : ");
-            String nomJ1  = sc.nextLine();
+            partieDeuxJoueurs(sc, sc2);
+        }
+    }
 
-            System.out.println(nomJ1.toUpperCase()+ " saisissez les informations pour votre grille : ");
-            Grille g1 = creerGrille(sc);
-            j1 = new Joueur(g1);
-            j1.setNomJoueur(nomJ1);
+
+    /**
+     * Methode permettant d'eviter la duplication de code pour une partie a deux joueurs
+     * @param sc Scanner 1 permettant la saisie clavier
+     * @param sc2 Scanner 2 permettant la saisie clavier
+     * @throws GrilleException
+     * @throws CaseException
+     * @throws BateauException
+     * @throws IOException
+     */
+    public void partieDeuxJoueurs(Scanner sc, Scanner sc2) throws GrilleException, CaseException, BateauException, IOException {
+        System.out.print("Joueur 1, entrez votre nom : ");
+        String nomJ1  = sc.nextLine();
+
+        System.out.println(nomJ1.toUpperCase()+ " saisissez les informations pour votre grille : ");
+        Grille g1 = creerGrille(sc);
+        j1 = new Joueur(g1);
+        j1.setNomJoueur(nomJ1);
+        System.out.println(g1.toString());
+
+        System.out.print("Joueur 2, entrez votre nom : ");
+        String nomJ2  = sc2.nextLine();
+        System.out.println(nomJ2.toUpperCase()+ " saisissez les informations pour votre grille : ");
+        Grille g2 = creerGrille(sc);
+        System.out.println(g2.toString());
+        j2 = new Joueur(g2);
+        j2.setNomJoueur(nomJ2);
+
+        System.out.println(nomJ1.toUpperCase()+" placez vos batteaux : ");
+        saisieBateau(sc,j1);
+
+        System.out.println(nomJ2.toUpperCase()+" placez vos batteaux : ");
+        saisieBateau(sc,j2);
+
+        while(!j1.perdu() && !j2.perdu()){
+            System.out.print(nomJ1.toUpperCase()+" : Entrez le x pour tirer : ");
+            int x1 = sc.nextInt();
+            System.out.print(nomJ1.toUpperCase()+" : Entrez le y pour tirer : ");
+            int y1 = sc.nextInt();
+            if (x1>=0 && x1<j2.getGrille().getLargeur() && y1>=0 && y1<j2.getGrille().getHauteur()){
+                j1.tirer(j2, j2.getGrille().gettCases()[x1][y1]);
+            }else {
+                j1.retirer(j2,sc);
+            }
+            System.out.println(g2.toString());
+            System.out.print(nomJ2.toUpperCase()+" : Entrez le x pour tirer : ");
+            int x2 = sc.nextInt();
+            System.out.print(nomJ2.toUpperCase()+" : Entrez le y pour tirer : ");
+            int y2 = sc.nextInt();
+            if (x2>=0 && x2<j1.getGrille().getLargeur() && y2>=0 && y2<j1.getGrille().getHauteur()){
+                j2.tirer(j1, j1.getGrille().gettCases()[x2][y2]);
+            }else {
+                j2.retirer(j1,sc);
+            }
             System.out.println(g1.toString());
 
-            System.out.print("Joueur 2, entrez votre nom : ");
-            String nomJ2  = sc2.nextLine();
-            System.out.println(nomJ2.toUpperCase()+ " saisissez les informations pour votre grille : ");
-            Grille g2 = creerGrille(sc);
-            System.out.println(g2.toString());
-            j2 = new Joueur(g2);
-            j2.setNomJoueur(nomJ2);
-
-            System.out.println(nomJ1.toUpperCase()+" placez vos batteaux : ");
-            saisieBateau(sc,j1);
-
-            System.out.println(nomJ2.toUpperCase()+" placez vos batteaux : ");
-            saisieBateau(sc,j2);
-
-            while(!j1.perdu() && !j2.perdu()){
-                System.out.print(nomJ1.toUpperCase()+" : Entrez le x pour tirer : ");
-                int x1 = sc.nextInt();
-                System.out.print(nomJ1.toUpperCase()+" : Entrez le y pour tirer : ");
-                int y1 = sc.nextInt();
-                if (x1>=0 && x1<j2.getGrille().getLargeur() && y1>=0 && y1<j2.getGrille().getHauteur()){
-                    j1.tirer(j2, j2.getGrille().gettCases()[x1][y1]);
-                }else {
-                    j1.retirer(j2,sc);
-                }
-                System.out.println(g2.toString());
-
-
-                System.out.print(nomJ2.toUpperCase()+" : Entrez le x pour tirer : ");
-                int x2 = sc.nextInt();
-                System.out.print(nomJ2.toUpperCase()+" : Entrez le y pour tirer : ");
-                int y2 = sc.nextInt();
-                if (x2>=0 && x2<j1.getGrille().getLargeur() && y2>=0 && y2<j1.getGrille().getHauteur()){
-                    j2.tirer(j1, j1.getGrille().gettCases()[x2][y2]);
-                }else {
-                    j2.retirer(j1,sc);
-                }
-                System.out.println(g1.toString());
-
-
-                if (j1.perdu()){
-                    break;
-                }
-                if (j2.perdu()){
-                    break;
-                }
-                System.out.println("Voulez-vous sauvegarder et quitter votre partie ? (Oui(o) / Non(n) : ");
-                boolean sauvegarder=false;
-                if (sc2.nextLine().toUpperCase().compareTo("OUI")==0){
-                    sauvegarder=true;
-                }
-                if (sauvegarder){
-                    sauvegarderDeuxJoueurs("saveDeuxJoueurs",this);
-                    break;
-                }
-            }
             if (j1.perdu()){
-                System.out.println(nomJ2.toUpperCase()+" a gagne !");
+                break;
             }
             if (j2.perdu()){
-                System.out.println(nomJ1.toUpperCase()+" a gagne !");
+                break;
             }
-            sc.close();
-            sc2.close();
+            System.out.println("Voulez-vous sauvegarder et quitter votre partie ? (Oui(o) / Non(n) : ");
+            boolean sauvegarder=false;
+            if (sc2.nextLine().toUpperCase().compareTo("OUI")==0){
+                sauvegarder=true;
+            }
+            if (sauvegarder){
+                sauvegarderDeuxJoueurs("saveDeuxJoueurs",this);
+                break;
+            }
         }
+        if (j1.perdu()){
+            System.out.println(nomJ2.toUpperCase()+" a gagne !");
+        }
+        if (j2.perdu()){
+            System.out.println(nomJ1.toUpperCase()+" a gagne !");
+        }
+        sc.close();
+        sc2.close();
     }
 
 
