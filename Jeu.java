cd19bd24b591aa783962dd80ef85642ba3a93918
Julien NOEL <juliennoel9@gmail.com>
2019-04-04 14:19:25 +0200

    Gestion de l'exception InputMismatchException dans le main

--- Jeu.java
+++ Jeu.java
@@ -1,4 +1,5 @@
 import java.io.Serializable;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -8,8 +9,19 @@ import java.util.Scanner;
 public class Jeu implements Serializable {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("A combien de joueur voulez vous jouez ? (1 ou 2) : ");
-        int choix = sc.nextInt();
+        boolean ok = false;
+        int choix=1;
+        while (!ok){
+            try {
+                System.out.print("A combien de joueur voulez vous jouez ? (1 ou 2) : ");
+                choix = sc.nextInt();
+                sc.nextLine();
+                ok=true;
+            }catch (InputMismatchException ime){
+                System.out.println("Erreur, entrez un entier (1 ou 2) : ");
+                sc.nextLine();
+            }
+        }
         switch (choix){
             case 1 :
                 MonoJoueur partieMonoJoueur = new MonoJoueur();
