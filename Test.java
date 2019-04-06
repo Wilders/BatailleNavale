62caabc345078f4d4ec388cd556a5ca7df387c0a
Jules Sayer <jules.sayer7@etu.univ-lorraine.fr>
2019-04-06 17:35:02 +0200

    Ajout des tests pour les constructeurs et méthodes des différents bateaux

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Test {
@@ -75,4 +76,202 @@ public class Test {
         }
 	}
 	
+	/**
+	 * Test de la classe Torpilleur (les tests Constructeurs
+	 * testent aussi les méthodes addCase et addMultipleCase
+	 * car ils font appel à ces méthodes)
+	 */
+	
+	@org.junit.Test(expected = BateauException.class)
+	public void testConstructeurTorpilleur1() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(1243, 123);
+		Torpilleur t = new Torpilleur(g, true, c);
+		Torpilleur t2 = new Torpilleur(g, true, c2);
+	}
+	
+	@org.junit.Test
+	public void testConstructeurTorpilleur2() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Torpilleur t = new Torpilleur(g, true, c);
+		ArrayList<Case> list = new ArrayList<Case>();
+		list.add(g.gettCases()[c.getX()][c.getY()]);
+		assertEquals("Devrait etre Torpilleur", "Torpilleur", t.getNom());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()].getDispo());
+		assertEquals("La liste devrait avoir les bonnes positions", list, t.getlPosition());
+	}
+	
+	/**
+	 * Test de la classe ContreTorpilleur (les tests Constructeurs
+	 * testent aussi les méthodes addCase et addMultipleCase
+	 * car ils font appel à ces méthodes)
+	 */
+	
+	@org.junit.Test(expected = BateauException.class)
+	public void testConstructeurContreTorpilleur1() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(1243, 123);
+		ContreTorpilleur ct = new ContreTorpilleur(g, true, c);
+		ContreTorpilleur ct2 = new ContreTorpilleur(g, true, c2);
+	}
+	
+	@org.junit.Test
+	public void testConstructeurContreTorpilleur2() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(70,50);
+		ContreTorpilleur ct = new ContreTorpilleur(g, true, c);
+		ContreTorpilleur ct2 = new ContreTorpilleur(g, false, c2);
+		ArrayList<Case> list = new ArrayList<Case>();
+		ArrayList<Case> list2 = new ArrayList<Case>();
+		list2.add(g.gettCases()[c2.getX()][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+1][c2.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()+1]);
+		assertEquals("Devrait etre Contre Torpilleur", "Contre Torpilleur", ct.getNom());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+1].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+1][c2.getY()].getDispo());
+		assertEquals("La liste devrait avoir les bonnes positions", list, ct.getlPosition());
+		assertEquals("La liste devrait avoir les bonnes positions", list2, ct2.getlPosition());
+	}
+	
+	/**
+	 * Test de la classe SousMarin (les tests Constructeurs
+	 * testent aussi les méthodes addCase et addMultipleCase
+	 * car ils font appel à ces méthodes)
+	 */
+	
+	@org.junit.Test(expected = BateauException.class)
+	public void testConstructeurSousMarin1() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(1243, 123);
+		SousMarin sm = new SousMarin(g, true, c);
+		SousMarin sm2 = new SousMarin(g, true, c2);
+	}
+	
+	@org.junit.Test
+	public void testConstructeurSousMarin2() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(70,50);
+		SousMarin sm = new SousMarin(g, true, c);
+		SousMarin sm2 = new SousMarin(g, false, c2);
+		ArrayList<Case> list = new ArrayList<Case>();
+		ArrayList<Case> list2 = new ArrayList<Case>();
+		list2.add(g.gettCases()[c2.getX()][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+1][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+2][c2.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()+1]);
+		list.add(g.gettCases()[c.getX()][c.getY()+2]);
+		assertEquals("Devrait etre Sous Marin", "Sous Marin", sm.getNom());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+1].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+2].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+1][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+2][c2.getY()].getDispo());
+		assertEquals("La liste devrait avoir les bonnes positions", list, sm.getlPosition());
+		assertEquals("La liste devrait avoir les bonnes positions", list2, sm2.getlPosition());
+	}
+	
+	/**
+	 * Test de la classe Croiseur (les tests Constructeurs
+	 * testent aussi les méthodes addCase et addMultipleCase
+	 * car ils font appel à ces méthodes)
+	 */
+	
+	@org.junit.Test(expected = BateauException.class)
+	public void testConstructeurCroiseur1() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(1243, 123);
+		Croiseur cr = new Croiseur(g, true, c);
+		Croiseur cr2 = new Croiseur(g, true, c2);
+	}
+	
+	@org.junit.Test
+	public void testConstructeurCroiseur2() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(70,50);
+		Croiseur cr = new Croiseur(g, true, c);
+		Croiseur cr2 = new Croiseur(g, false, c2);
+		ArrayList<Case> list = new ArrayList<Case>();
+		ArrayList<Case> list2 = new ArrayList<Case>();
+		list2.add(g.gettCases()[c2.getX()][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+1][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+2][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+3][c2.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()+1]);
+		list.add(g.gettCases()[c.getX()][c.getY()+2]);
+		list.add(g.gettCases()[c.getX()][c.getY()+3]);
+		assertEquals("Devrait etre Croiseur", "Croiseur", cr.getNom());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+1].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+2].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+3].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+1][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+2][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+3][c2.getY()].getDispo());
+		assertEquals("La liste devrait avoir les bonnes positions", list, cr.getlPosition());
+		assertEquals("La liste devrait avoir les bonnes positions", list2, cr2.getlPosition());
+	}	
+	
+	/**
+	 * Test de la classe PorteAvions (les tests Constructeurs
+	 * testent aussi les méthodes addCase et addMultipleCase
+	 * car ils font appel à ces méthodes)
+	 */
+	
+	@org.junit.Test(expected = BateauException.class)
+	public void testConstructeurPorteAvions1() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(1243, 123);
+		PorteAvions pa = new PorteAvions(g, true, c);
+		PorteAvions pa2 = new PorteAvions(g, true, c2);
+	}
+	
+	@org.junit.Test
+	public void testConstructeurPorteAvions2() throws BateauException, CaseException, GrilleException {
+		Grille g = new Grille(150, 200);
+		Case c = new Case(111,1);
+		Case c2 = new Case(70,50);
+		PorteAvions pa = new PorteAvions(g, true, c);
+		PorteAvions pa2 = new PorteAvions(g, false, c2);
+		ArrayList<Case> list = new ArrayList<Case>();
+		ArrayList<Case> list2 = new ArrayList<Case>();
+		list2.add(g.gettCases()[c2.getX()][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+1][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+2][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+3][c2.getY()]);
+		list2.add(g.gettCases()[c2.getX()+4][c2.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()]);
+		list.add(g.gettCases()[c.getX()][c.getY()+1]);
+		list.add(g.gettCases()[c.getX()][c.getY()+2]);
+		list.add(g.gettCases()[c.getX()][c.getY()+3]);
+		list.add(g.gettCases()[c.getX()][c.getY()+4]);
+		assertEquals("Devrait etre Porte avions", "Porte avions", pa.getNom());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+1].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+2].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+3].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c.getX()][c.getY()+4].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+1][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+2][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+3][c2.getY()].getDispo());
+		assertEquals("La case devrait ne pas être disponible", false, g.gettCases()[c2.getX()+4][c2.getY()].getDispo());
+		assertEquals("La liste devrait avoir les bonnes positions", list, pa.getlPosition());
+		assertEquals("La liste devrait avoir les bonnes positions", list2, pa2.getlPosition());
+	}
 }
